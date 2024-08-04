96fe89501ee9cc57afa80dd55d08b3045b455b6e
RishiMishra3004 <12rishi2003@gmail.com>
2024-08-04 15:40:58 +0530

    Frontend Finished

--- src/App.js
+++ src/App.js
@@ -1,119 +1,106 @@
+import React, {Component} from 'react';
+import Clarifai from 'clarifai';
+import Signin from './components/Signin/Signin';
+import Register from './components/Register/Register';
 import Navigation from './components/navigation/Navigation';
+import FaceRecognition from './components/FaceRecognition/FaceRecognition';
 import Logo from './components/logo/Logo';
 import Rank from './components/Rank/Rank';
 import ImageLinkForm from './components/ImageLinkForm/ImageLinkForm';
+import ParticlesBg from 'particles-bg'
 import './App.css';
 
-import {useEffect, useState } from "react";
-import Particles, { initParticlesEngine } from "@tsparticles/react";
-import { loadSlim } from "@tsparticles/slim";
+window.process = {
+      ...window.process,
+    };
 
-const ParticleOptions = {
-  // background: {
-  //     color: {
-  //         value: "#0d47a1",
-  //     },
-  // },
-  fpsLimit: 120,
-  interactivity: {
-      events: {
-          onClick: {
-              enable: true,
-              mode: "push",
-          },
-          onHover: {
-              enable: true,
-              mode: "repulse",
-          },
-          resize: true,
-      },
-      modes: {
-          push: {
-              quantity: 4,
-          },
-          repulse: {
-              distance: 200,
-              duration: 0.4,
-          },
-      },
-  },
-  particles: {
-      color: {
-          value: "#ffffff",
-      },
-      links: {
-          color: "#ffffff",
-          distance: 150,
-          enable: true,
-          opacity: 0.5,
-          width: 1,
-      },
-      move: {
-          direction: "none",
-          enable: true,
-          outModes: {
-              default: "bounce",
-          },
-          random: false,
-          speed: 3,
-          straight: false,
-      },
-      number: {
-          density: {
-              enable: true,
-              area: 800,
-          },
-          value: 200,
-      },
-      opacity: {
-          value: 0.5,
-      },
-      shape: {
-          type: "circle",
-      },
-      size: {
-          value: { min: 1, max: 5 },
-      },
-  },
-  detectRetina: true,
+const app = new Clarifai.App({
+    apiKey: 'cd9dc2dc716f4584b69c1d85d35220f9',
+});
+
+const model = {
+    id: 'face-detection',
+    name: 'face-detection',
+    version: '6dc7e46bc9124c5c8824be4822abe105',
+    type: 'visual-detector',
 }
 
-const App = () => {
-  const [init, setInit] = useState(false);
+class App extends Component {
+    constructor(){
+        super();
+        this.state = {
+            input: '',
+            imageUrl: '',
+            box: {},
+            route: 'signin',
+            isSignedIn: false,
+        }
+    }
 
-  // this should be run only once per application lifetime
-  useEffect(() => {
-      initParticlesEngine(async (engine) => {
+    calculateFaceLocation = (data) => {
+        const clarifaiFace = data.outputs[0].data.regions[0].region_info.bounding_box;
+        const image = document.getElementById('inputimage');
+        const width = Number(image.width);
+        const height = Number(image.height);
 
-          await loadSlim(engine);
-          //await loadBasic(engine);
-      }).then(() => {
-          setInit(true);
-      });
-  }, []);
+        return {
+            leftcol: clarifaiFace.left_col * width,
+            toprow: clarifaiFace.top_row * height,
+            rightcol: width - (clarifaiFace.right_col * width),
+            bottomrow: height - (clarifaiFace.bottom_row * height)
+        }
+    }
 
-  const particlesLoaded = (container) => {
-      console.log(container);
-  };
+    displayFaceBox = (box) => {
+        this.setState({box: box});
+    }
+     
+    onInputChange = (event) => {
+        this.setState({input: event.target.value});
+    }
 
-  return (
-      init && (
-        <div className="App">
-          <Particles className='particles'
-              id="tsparticles"
-              particlesLoaded={particlesLoaded}
-              options={ParticleOptions}
-          />
-          <Navigation />
-          <Logo />
-          <Rank />
-          <ImageLinkForm />
-          {/* <FaceRecognition /> */}
-        </div>
-          
-      )
-  );
+    onButtonSubmit = () => {
+        this.setState({imageUrl: this.state.input});
+        app.models.predict(model, this.state.input)
+        .then(response => this.displayFaceBox(this.calculateFaceLocation(response)))
+        .catch(err => console.log(err));
+    }
+    
+    onRouteChange = (route) => {
+        if(route === 'signout'){
+            this.setState({isSignedIn: false})
+        }
+        else if(route === 'home'){
+            this.setState({isSignedIn: true});
+        }
+        this.setState({route: route});
+    }
+
+    render() {
+       const { isSignedIn, imageUrl, route, box } = this.state;
+        return (
+            <div className="App">
+                <ParticlesBg color="#FFFFFF" num={150} type="cobweb" bg={true} />
+                <Navigation isSignedIn={isSignedIn} onRouteChange={this.onRouteChange}/>
+                { route === 'home' 
+                    ?
+                    <div> 
+                        <Logo />
+                        <Rank />
+                        <ImageLinkForm onInputChange={this.onInputChange}
+                                    onButtonSubmit={this.onButtonSubmit}/>
+                        <FaceRecognition box={box} 
+                                        imageUrl={imageUrl} />
+                    </div>
+                    
+                    : (route === 'signin'
+                        ? <Signin onRouteChange={this.onRouteChange} />
+                        : <Register onRouteChange={this.onRouteChange} />
+                    )
+                }
+            </div>
+        );
+    }  
 };
-
 
 export default App;
